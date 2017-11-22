98525f62295c4219c4e1b313b874cbc293284cf6
arioscen <arioscen@gmail.com>
2017-11-22 17:53:11 +0800

    20171122

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, redirect, url_for
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/Index.html')
+def index():
+    return render_template('Index.html')
+
+
+@app.route('/')
+def main():
+    return redirect(url_for('index'))
+
+
+@app.route('/profile')
+def profile():
+
+    return render_template('profile.html')
+
+
+if __name__ == '__main__':
+    app.run()
