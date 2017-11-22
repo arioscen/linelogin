d5a4fb5e33387e60a174353967617d6d340be7d0
arioscen <arioscen@gmail.com>
2017-11-22 18:12:24 +0800

    20171122

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 import requests
+import json
 
 app = Flask(__name__)
 
@@ -16,7 +17,25 @@ def main():
 
 @app.route('/profile')
 def profile():
-    return render_template('profile.html')
+    authorize_url = "https://api.line.me/oauth2/v2.1/token"
+    session = requests.Session()
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {
+        "grant_type": "authorization_code",
+        "code": request.args.get('code'),
+        "redirect_uri": "http://localhost:5000/Index.html",
+        "client_id": "1547929126",
+        "client_secret": "b4d671b90cb6b791c12f2fc3380edb8e"
+    }
+    response = session.post(authorize_url, headers=headers, data=data)
+    access_token = json.loads(response.text)['access_token']
+
+    profile_url = "https://api.line.me/v2/profile"
+    profile_text = requests.get(profile_url, headers={"Authorization": "Bearer %s" % access_token}).text
+
+    return profile_text
 
 
 if __name__ == '__main__':
